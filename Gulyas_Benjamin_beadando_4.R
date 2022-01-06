2224d36afb90e6dcf509a37af3836d1ebc3287a3
gulyasbeni1 <96257114+gulyasbeni1@users.noreply.github.com>
2022-01-06 10:45:55 +0100

    Update Gulyas_Benjamin_beadando_4.R

--- Gulyas_Benjamin_beadando_4.R
+++ Gulyas_Benjamin_beadando_4.R
@@ -2,56 +2,46 @@
 #nevet és kimenetként a két változóra készít egy scatterplot-t, amit kirajzol. Opcionálisan lehessen 
 #tetszőleges "title"-t hozzáadni, illetve képként menteni is az eredményt.
 
+
 library(utils )
-
-graf <- function ( adat , var1, var2 , cím ) {
-
-  if (is.data.frame(adat)) { 
-    print("A scatterplot pedig...")
-  } else  {  stop("Nem data.frame a bemenet") }
-
-  png(file = "beadandó_scatterplot.png")
   
-  plot(x = adat[,var1] , y = adat[, var2] ,
-       xlab = var1,
-       ylab = var2,
-       xlim = c(min(adat[,var1]), max(adat[,var1])),
-       ylim = c(min(adat[,var2]), max(adat[,var2])),	
-       main = cím,
-       col= "red" )
-  
-  dev.off()
-}
- 
-##ellenőrzés
- 
-graf(mtcars, "wt", "mpg", "Title")
+graf2 <- function ( adat , var1, var2 , cím , kiírás= "nem") {
+    
+    if (is.data.frame(adat)) { 
+      print("A scatterplot pedig...")  }
+    else  {  stop("Nem data.frame a bemenet") }
 
-graf(iris, "Sepal.Length", "Sepal.Width", cím= "NA HAJRÁ")
+    if (!any(class(adat[,var1]) == c( "numeric", "integer", "factor"))) {
+      stop("Az első változó nem szám formátumú") }
+    
+    if (!any(class(adat[,var2]) == c( "numeric", "integer", "factor"))) {
+      stop("A második változó nem szám formátumú") }
 
--------------------------
-#GGPLOT-os verzió
-  
-library(ggplot2)
-library(hrbrthemes)
+    
+    p<-  plot(x = adat[,var1] , y = adat[, var2] ,
+              xlab = var1,
+              ylab = var2,
+              xlim = c(min(adat[,var1]), max(adat[,var1])),
+              ylim = c(min(adat[,var2]), max(adat[,var2])),	
+              main = cím,
+              col= "red" )
+    
+    print(p)
+    
+    if (kiírás == "igen")  {
+      png(file = "beadandó_scatterplot.png")
+      plot(x = adat[,var1] , y = adat[, var2] ,
+           xlab = var1,
+           ylab = var2,
+           xlim = c(min(adat[,var1]), max(adat[,var1])),
+           ylim = c(min(adat[,var2]), max(adat[,var2])),	
+           main = cím,
+           col= "red" )
+      dev.off()              
+    }
+  }
 
-graf2 <- function ( adat , var1, var2 , cím ) {
-  
-  if (is.data.frame(adat)) { 
-    print("A scatterplot pedig...")
-  } else  {  stop("Nem data.frame a bemenet") }
-  
-  png(file = "beadandó_scatterplot.png")
-  
-  kep2 <- ggplot(iris, aes_string(x=var1 , y=var2  )) + 
-    geom_point(size=3) +
-    theme_ipsum()
-print(kep2)
-  
-  dev.off()
-}
+################ Ellenőrzés
+graf2(mtcars, "wt", "mpg", "Title", "igen")
 
-#Ellenőrzés
-graf2(mtcars, "mpg", "cyl", "Cím")
-
-graf2(iris, "Sepal.Length", "Sepal.Width", cím= "NA HAJRÁ")
+graf2(iris, "Sepal.Length", "Sepal.Width", cím= "NA HAJRÁ", "igen")
